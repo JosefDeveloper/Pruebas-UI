f7983cce3587df1fdd40ef23151332772a9082ce
JosefDeveloper <jfelix0107@gmail.com>
2022-07-10 19:18:03 -0500

    Click

---
+++ cypress/e2e/3-platzi-course/interactuandoConElementos.cy.js
@@ -0,0 +1,38 @@
+describe('Interactuando con los elementos', () => {
+	it('Click normal', () => {
+		cy.visit('/buttons')
+		cy.get('button').eq(3).click()
+		cy.get('#dynamicClickMessage')
+			.should('be.visible')
+			.and('contain', 'You have done a dynamic click')
+	})
+
+	it('Double Click', () => {
+		cy.visit('/buttons')
+		cy.get('#doubleClickBtn').dblclick()
+		cy.get('#doubleClickMessage')
+			.should('be.visible')
+			.and('contain', 'You have done a double click')
+	})
+
+	it('Right Click', () => {
+		cy.visit('/buttons')
+		cy.get('#rightClickBtn').rightclick()
+		cy.get('#rightClickMessage')
+			.should('be.visible')
+			.and('contain', 'You have done a right click')
+	})
+
+	it('Force Click', () => {
+		cy.visit('/dynamic-properties')
+		// cy.get('#enableAfter').click({ timeout: 0 })
+		cy.get('#enableAfter').click({ timeout: 0, force: true })
+	})
+
+	it.only('Click por posición', () => {
+		cy.visit('/buttons')
+		cy.get('button').eq(3).parent().parent().click('topRight')
+		cy.get('button').eq(3).parent().parent().click('bottomLeft')
+		cy.get('button').eq(3).parent().parent().click(5, 60)
+	})
+})
