2bf978d34acf5d94c213d4d9e003ee8c822ac61d
JosefDeveloper <jfelix0107@gmail.com>
2022-07-15 10:31:26 -0500

    Click, Inputs, Checkbox, RadioButtons, GetInfo

--- cypress/e2e/3-platzi-course/interactuandoConElementos.cy.js
+++ cypress/e2e/3-platzi-course/interactuandoConElementos.cy.js
@@ -1,4 +1,6 @@
 describe('Interactuando con los elementos', () => {
+	let texto
+
 	it('Click normal', () => {
 		cy.visit('/buttons')
 		cy.get('button').eq(3).click()
@@ -29,10 +31,57 @@ describe('Interactuando con los elementos', () => {
 		cy.get('#enableAfter').click({ timeout: 0, force: true })
 	})
 
-	it.only('Click por posición', () => {
+	it('Click por posición', () => {
 		cy.visit('/buttons')
 		cy.get('button').eq(3).parent().parent().click('topRight')
 		cy.get('button').eq(3).parent().parent().click('bottomLeft')
 		cy.get('button').eq(3).parent().parent().click(5, 60)
+	})
+
+	it('Input type text', () => {
+		cy.visit('/automation-practice-form')
+		cy.get('#firstName').type('Josef')
+		cy.get('#lastName').type('Developer')
+
+		cy.get('#firstName').type('Josef')
+
+		cy.get('#firstName').type('{selectAll}{backspace}')
+		cy.get('#firstName').type('Otro nombre')
+		cy.get('#firstName').clear()
+	})
+
+	it('Checkbox y Radio buttons', () => {
+		cy.visit('/automation-practice-form')
+		// cy.get('#gender-radio-1').click()
+		// cy.get('#gender-radio-1').click({ force: true })
+		// cy.get('#gender-radio-1').check({ force: true })
+		cy.get('label[for="gender-radio-1"]').click()
+
+		// cy.get('#hobbies-checkbox-3').check({ force: true })
+		// cy.get('#hobbies-checkbox-3').uncheck({ force: true })
+		cy.get('label[for="hobbies-checkbox-3"]').click()
+		cy.get('label[for="hobbies-checkbox-3"]').click()
+	})
+
+	it.only('Extrayendo información', function () {
+		cy.visit('/automation-practice-form')
+		cy.get('#firstName').as('nombre')
+		cy.get('@nombre').type('Josef')
+
+		cy.get('@nombre').then(($nombre) => {
+			texto = $nombre.val()
+			expect(texto).to.equal('Josef')
+		})
+
+		cy.get('@nombre').invoke('val').should('equal', 'Josef')
+		cy.get('@nombre').invoke('val').as('nombreGlobal')
+	})
+
+	it.only('Compartiendo información', function () {
+		cy.visit('/automation-practice-form')
+		cy.get('#lastName').as('apellido')
+		cy.get('@apellido').type(texto)
+
+		cy.get('#firstName').type(this.nombreGlobal)
 	})
 })
